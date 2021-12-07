45d252c2026815f2a02b8b31fcd34e1456ff6eef
Chris Throup <chris@throup.eu>
2021-12-07 07:59:09 +0100

    2021 Day 07: The Treachery of Whales

---
+++ src/main/scala/eu/throup/aoc/year2021/day07/Day07.scala
@@ -0,0 +1,34 @@
+package eu.throup.aoc.year2021.day07
+
+import eu.throup.aoc.year2021.DayXX
+
+object Day07 extends DayXX {
+  override def part1(input: String) =
+    leastFuel(parseInput(input), identity)
+
+  override def part2(input: String) =
+    leastFuel(parseInput(input), triangleNumber)
+
+  def leastFuel(crabs: Array[Int], summer: Int => Int) =
+    (crabs.min to crabs.max)
+      .map(fuelForPosition(_, crabs, summer))
+      .min
+
+  def fuelForPosition(
+      pos: Int,
+      crabs: Array[Int],
+      summer: Int => Int
+  ): Int =
+    crabs
+      .map(_ - pos)
+      .map(_.abs)
+      .map(summer)
+      .sum
+
+  def triangleNumber(a: Int): Int = (a * (a + 1)) / 2
+
+  def parseInput(input: String): Array[Int] =
+    input.trim
+      .split(",")
+      .map(_.toInt)
+}
---
+++ src/test/scala/eu/throup/aoc/year2021/day07/Day07Spec.scala
@@ -0,0 +1,21 @@
+package eu.throup.aoc.year2021.day07
+
+import eu.throup.aoc.year2021.DayXXSpec
+
+class Day07Spec extends DayXXSpec {
+  override type returnType = Long
+  override val testObject = Day07
+
+  override def testInput1 = Map(
+    "sample input" -> (sampleInput, 37),
+    "puzzle input" -> (puzzleInput, 323647)
+  )
+
+  override def testInput2 = Map(
+    "sample input" -> (sampleInput, 168),
+    "puzzle input" -> (puzzleInput, 87640209)
+  )
+
+  def sampleInput = loadResource("sampleinput.txt")
+  def puzzleInput = loadResource("puzzleinput.txt")
+}
